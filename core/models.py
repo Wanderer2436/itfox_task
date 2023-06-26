212de60a3f58fd1fbfa183b19855d768d790f882
wandering24 <t.gaysin@itcodegroup.ru>
2023-06-26 17:09:24 +0500

    #5 Добавил админку

--- core/models.py
+++ core/models.py
@@ -28,17 +28,43 @@ class News(models.Model):
     )
     dc = models.DateTimeField('Дата новости', auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.title
+
+    @property
+    def likes_count(self) -> int:
+        return self.likes.count()
+
+    @property
+    def comments_count(self) -> int:
+        return self.comments.count()
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ('-dc',)
+
 
 class Comments(models.Model):
     author = models.ForeignKey(
         Users,
         verbose_name='Автор',
         on_delete=models.CASCADE,
+        related_name='comments',
     )
     news = models.ForeignKey(
         News,
         verbose_name='Новость',
         on_delete=models.CASCADE,
+        related_name='comments',
     )
     text = models.CharField('Текст комментария', max_length=255)
     dc = models.DateTimeField('Дата комментария', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.author}-{self.text}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-dc',)
